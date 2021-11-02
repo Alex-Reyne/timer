4694b8ed586ea027ce43b932b845f3d39b465b00
Alex-Reyne <creativereyne@gmail.com>
2021-11-02 13:59:55 -0600

    create timer that takes user input using promp-sync and sets timer (in seconds)

---
+++ timer1.js
@@ -0,0 +1,21 @@
+const prompt = require('prompt-sync')();
+
+const timer = function(seconds) {
+  const timerArr = [];
+  
+  for (const char of seconds) {
+    timerArr.push(Number(char));
+  }
+
+  for (const number of timerArr) {
+    if (number === 'NaN') {
+      null;
+    } else if (number > 0) {
+      setTimeout(() => process.stdout.write('\x07'), number * 1000);
+    }
+  }
+
+}
+
+timer(prompt('Set timers: '));
+
